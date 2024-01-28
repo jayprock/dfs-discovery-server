820626c248acf903b80cebfde9b5bd6a7f536baf
Joe Prochnow <jprochnow@neovera.com>
2024-01-27 20:33:46 -0500

    Initial discovery server

---
+++ src/test/java/com/bitbus/dfsdiscoveryserver/DfsDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bitbus.dfsdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfsDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
